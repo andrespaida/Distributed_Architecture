cc1818050dc9790669e35394e08111f7bd2fc371
andrespaida <jordanpaida.bj@hotmail.com>
2025-05-15 20:42:23 -0500

    five commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function goToPage(platform) {
   const urls = {
     vercel: "https://project-vercel-five.vercel.app/",
     railway: "https://holamundopy-production.up.railway.app/",
-    digitalocean: "https://tu-digitalocean-app.com",
+    digitalocean: "https://hola-mundo-zlywd.ondigitalocean.app/",
     heroku: "https://nodeheroku-68e9898d21c7.herokuapp.com/"
   };
 
