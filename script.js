8a7175f657b35a63b0844158c2149a147b9b51ca
andrespaida <jordanpaida.bj@hotmail.com>
2025-05-18 21:30:24 -0500

    six commit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function goToPage(platform) {
     vercel: "https://project-vercel-five.vercel.app/",
     railway: "https://holamundopy-production.up.railway.app/",
     digitalocean: "https://hola-mundo-zlywd.ondigitalocean.app/",
-    heroku: "https://nodeheroku-68e9898d21c7.herokuapp.com/"
+    heroku: "https://nodeheroku-68e9898d21c7.herokuapp.com/",
+    EC2: "http://54.205.65.51/"
   };
 
   window.location.href = urls[platform];
